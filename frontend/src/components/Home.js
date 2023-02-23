f6cc1c01a9f619b72f9a555aa941fb55740bb101
Rak1800 <104507574+Rak1800@users.noreply.github.com>
2023-02-23 17:41:13 +0530

    Update Home.js

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+            <button className='logbtn' type='button'><Link to="/login">signUp</Link></button>
+            <button className='logbtn' type='button'><Link to="/login">login</Link></button>
+    </div>
   )
 }
