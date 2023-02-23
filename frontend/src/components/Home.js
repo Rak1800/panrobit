477febf6137278e0c9b9e3db9f0780dae1996ac9
RAKESH KUMAR <rakeshkumar756413936@gmail.com>
2023-02-23 18:46:36 +0530

    frontend

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link} from 'react-router-dom'
 
 export default function Home() {
   return (
     <div>
-            <button className='logbtn' type='button'><Link to="/login">signUp</Link></button>
-            <button className='logbtn' type='button'><Link to="/login">login</Link></button>
+       <button className='logbtn' type='button'><Link to="/signup">signUp</Link></button>
+        <button className='logbtn' type='button'><Link to="/login">login</Link></button>
     </div>
   )
 }
